3fadb6938678e91215668984d821f20ea7f262a3
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-06-19 22:49:08 +0100

    +

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ContactList from "../../components/ContactList/ContactList";
-import { fetchContacts } from "../../redux/contacts/contactsOps";
+import { fetchContacts } from "../../redux/contacts/operations";
 import { selectLoading } from "../../redux/contacts/selectors";
 
 export default function ContactsPage() {
