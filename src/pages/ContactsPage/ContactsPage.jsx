ebae39362adabd3c8cc04e1435a74d74877603ac
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-06-19 14:56:06 +0100

    +

---
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ContactList from "../../components/ContactList/ContactList";
+import { fetchContacts } from "../../redux/contacts/contactsOps";
+import { selectLoading } from "../../redux/contacts/selectors";
+
+export default function ContactsPage() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
---
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,9 @@
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
+
+export default function RegistrationForm() {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+}
