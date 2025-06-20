2b0a327963786410de808c07ad792140ad08b139
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-06-20 23:14:42 +0100

    +

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -62,7 +62,7 @@ export const refreshUser = createAsyncThunk(
 
     try {
       setAuthHeader(token);
-      const response = await axios.get('/users/me');
+      const response = await axios.get('/users/current');
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response?.data?.message || error.message);
